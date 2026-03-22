cbbf3d548fa0dbe1776da2c0d4cd247065450186
davidherascu-tx <david.herascu@gmail.com>
2026-03-21 22:34:50 -0500

    Foto updates

--- lib/photoData.ts
+++ lib/photoData.ts
@@ -9,25 +9,69 @@ export const photoData = [
         photos: [
           // Hier fügst du für jedes Bild eine Zeile mit src und description ein:
           { src: "/fotos/karate/karate/11_2019_scb_jukuren.jpg", description: "11/2019 SCB Jukuren gratulieren" },
-          { src: "/fotos/karate/karate/bild2.jpg", description: "Kata Unsu Vorführung in Berlin." },
-          { src: "/fotos/karate/karate/bild3.jpg", description: "Korrektur der Haltung bei den Meisterschaften." },
+          { src: "/fotos/karate/karate/11_2019_scb_pruefung_2_dan.jpg", description: "11/2019 Sechs SCB-Karateka bestehen die Prüfung zum 2. Dan" },
+          { src: "/fotos/karate/karate/11_2019_jkd_cup_sachsen_anhalt.jpg", description: "11/2019 JKD Cup in Sachsen Anhalt" },
+          { src: "/fotos/karate/karate/11_2019_shotokan_cup_berlin.jpg", description: "11/2019 Shotokan Cup Berlin" },
+          { src: "/fotos/karate/karate/12_2019_kazoku_cup_berlin.jpg", description: "12/2019 Kazoku Cup Berlin" },
+          { src: "/fotos/karate/karate/1985_training-_mit_genossen_aus_vietnam.jpg", description: "1985 Training mit Genossen aus Vietnam" },
+          { src: "/fotos/karate/karate/boy_san_jka_champion_kata_2009-min.jpg", description: "Boy-San (SCB) bei den JKA Championships / Tokyo 2009 - Kata" },
+          { src: "/fotos/karate/karate/boy_san_jka_champion_kumite_2009-min.jpg", description: "Boy-San (SCB) bei den JKA Championships / Tokyo 2009 - Kumite" },
+          { src: "/fotos/karate/karate/budo-min.jpg", description: "12/2001 BUDO-Demonstration in ausverkaufter Halle / SCB-Turngala im Sportforum" },
+          { src: "/fotos/karate/karate/davos-min.jpg", description: "10/2004 als Teilnehmer an der ITKF-WM in Davos/CH" },
+          { src: "/fotos/karate/karate/dsc01147-min.jpg", description: "Dojo im ehemaligen WR F.E.D." },
+          { src: "/fotos/karate/karate/fujinaga_leipzig-min.jpg", description: "1994 in Leipzig mit Fujinaga Sensei" },
+          { src: "/fotos/karate/karate/hart_aber_herzlich-min.jpg", description: "mit Kevin & Nathan - hart aber herzlich" }
         ]
       },
-      {
+      { 
         title: "SCB-Pfingst-LG mit Seto-Sensei",
         photos: [
-          { src: "/fotos/karate/pfingsten/seto1.jpg", description: "Seto-Sensei erklärt die Feinheiten der Kata." },
-          { src: "/fotos/karate/pfingsten/seto2.jpg", description: "Gruppenfoto nach dem anstrengenden Lehrgang." },
+          { src: "/fotos/karate/pfingsten/dsc00063-min.jpg", description: "Japanische Gäste im Sportforum" },
+          { src: "/fotos/karate/pfingsten/dsc00067-min.jpg", description: "Gruppenfoto mit Gästen" },
+          { src: "/fotos/karate/pfingsten/fj_seto_2008_klein-min.jpg", description: "Trainer - Kollegen" }
         ]
       },
       {
         title: "Otake Sensei in Schwerin - 2012",
-        photos: [] // Hier später deine Bilder einfügen
+        photos: [
+          { src: "/fotos/karate/otake/schwerin_danpruefung-min.jpg", description: "Bestandene Dan-Prüfung" },
+          { src: "/fotos/karate/otake/schwerin_otake_2012-min.jpg", description: "Teilnehmer beim Lehrgang" },
+          { src: "/fotos/karate/otake/schwerin_puerfungskommission-min.jpg", description: "Dan-Prüfungskommission" }
+        ] // Hier später deine Bilder einfügen
       },
-      { title: "Budapest 2010", photos: [] },
-      { title: "USA Reise 2010", photos: [] },
-      { title: "AJKA-EuroCup 2008", photos: [] },
-      { title: "WUKO-EM / Belgien", photos: [] }
+      { title: "Budapest 2010", photos: [
+          { src: "/fotos/karate/budapest/19_budapest-min.jpg", description: "Budapest" },   
+          { src: "/fotos/karate/budapest/20_jka-trainer-min.jpg", description: "JKA-Trainer" },
+          { src: "/fotos/karate/budapest/21_mit-naito-sensei-min.jpg", description: "mit Naito-Sensei" },
+          { src: "/fotos/karate/budapest/22_mit-sawada-sensei-min.jpg", description: "mit Sawada-Sensei" }
+      ] },
+      { title: "USA Reise 2010", photos: [
+          { src: "/fotos/karate/usa/1_abflug-txl-min.jpg", description: "Abflug TXL" },
+          { src: "/fotos/karate/usa/2_universal-studios-min.jpg", description: "Universal Studios" },
+          { src: "/fotos/karate/usa/3_hollywood-min.jpg", description: "Hollywood" },
+          { src: "/fotos/karate/usa/4_action-pur-min.jpg", description: "Action Pur" },
+          { src: "/fotos/karate/usa/5_camp-shotokan-min.jpg", description: "AJKA-Camp SHOTOKAN 08/2010" },
+          { src: "/fotos/karate/usa/6_medaillenausbeute-min.jpg", description: "SCB-Medaillengewinner" },
+          { src: "/fotos/karate/usa/7_just-for-fun-min.jpg", description: "Just for Fun" },
+          { src: "/fotos/karate/usa/8_mit-sensei-yabe-min.jpg", description: "mit Sensei Yabe" },
+          { src: "/fotos/karate/usa/9_scb-power-kick-min.jpg", description: "SCB-Power Kick" },
+          { src: "/fotos/karate/usa/10_mit-schwung-min.jpg", description: "mit Schwung" },
+          { src: "/fotos/karate/usa/11_und-sprung-min.jpg", description: "Und Sprung" },
+          { src: "/fotos/karate/usa/12_unterwegs-min.jpg", description: "Unterwegs" },
+          { src: "/fotos/karate/usa/13_grand-canyon-ranch-min.jpg", description: "Grand Canyon Ranch" },
+          { src: "/fotos/karate/usa/14_las-vegas-min.jpg", description: "Las Vegas" },
+          { src: "/fotos/karate/usa/15_bei-hooters-min.jpg", description: "Bei Hooters" },
+          { src: "/fotos/karate/usa/16_am-venice-beach-min.jpg", description: "Am Venice Beach" }
+      ] },
+      { title: "AJKA-EuroCup 2008", photos: [
+        { src: "/fotos/karate/ajkaeurocup/100_0482-min.jpg", description: "1. Platz Team Kata" },
+        { src: "/fotos/karate/ajkaeurocup/100_0498-min.jpg", description: "2. Platz Kata AKL ü. 35" },
+        { src: "/fotos/karate/ajkaeurocup/100_0508-min.jpg", description: "mit dem SRD-Team" }
+              ] },
+      { title: "WUKO-EM / Belgien", photos: [
+        { src: "/fotos/karate/wuko/230508-091606-min.jpg", description: "WUKO-EM / Belgien" },
+        { src: "/fotos/karate/wuko/dsc00729-min.jpg", description: "Coach und Kämpfer" }
+      ] }
     ]
   },
   {
@@ -36,11 +80,46 @@ export const photoData = [
       { 
         title: "Japan 2006", 
         photos: [
-          { src: "/fotos/japan/2006/tokyo.jpg", description: "Ankunft in Tokyo am Abend." },
+          { src: "/fotos/japan/2006/100_0442-min.jpg", description: "Abendessen nach dem Training mit Seto-Sensei" },
+          { src: "/fotos/japan/2006/100_0446-min.jpg", description: "am Grab der Familie Fujinaga / 15.10.2006" },
+          { src: "/fotos/japan/2006/dscf0055-min.jpg", description: "als Samurai in Nikko" },
+          { src: "/fotos/japan/2006/dscf0057-min.jpg", description: "mit \"Freund und Helfer\" in Nikko" }, 
+          { src: "/fotos/japan/2006/japan_079-min.jpg", description: "Gruppenfoto mit Seto-Sensei / Tokyo 10/2006" },
+          { src: "/fotos/japan/2006/japan_183-min.jpg", description: "am Grab der Familie Fujinaga / 15.10.2006" },
+          { src: "/fotos/japan/2006/japan2_029-min.jpg", description: "im JKA-Honbu-Dojo mit Peggy F." }, 
+          { src: "/fotos/japan/2006/japan2_433-min.jpg", description: "in der Burg Himeji" },
+          { src: "/fotos/japan/2006/japan2_482-min.jpg", description: "auf MIYAJIMA" }
         ] 
       },
-      { title: "Japan 04/2009", photos: [] },
-      { title: "Japan 04/2015", photos: [] }
+      { title: "Japan 04/2009", photos: [
+          { src: "/fotos/japan/2009/100_1512-min.jpg", description: "Auf dem Weg nach TSUMAGO: Kirschblüten Tricolor" },
+          { src: "/fotos/japan/2009/100_1527-min.jpg", description: "Abendessen im RYOKAN \"DAI KICHI\"" },
+          { src: "/fotos/japan/2009/100_1595-min.jpg", description: "Frühling auf MIYAJIMA" },
+          { src: "/fotos/japan/2009/100_1598-min.jpg", description: "Immer wieder schön: Das Tor" },
+          { src: "/fotos/japan/2009/100_1695-min.jpg", description: "Die Brücke von IWAKUNI" },
+          { src: "/fotos/japan/2009/dsc00298_1-min.jpg", description: "\"Wochenend-Dojo\" in NARA: schön wärs." },
+          { src: "/fotos/japan/2009/dscf0584-min.jpg", description: "Besuch der Schwertschmiede in OSAFUNE (inkl. Schwertmuseum und Shop)" },
+          { src: "/fotos/japan/2009/dscf0613-min.jpg", description: "Die schwarze Burg von OKAYAMA)" },
+          { src: "/fotos/japan/2009/seto_dojo-min.jpg", description: "09.04.2009, Seto-Sensei bat mich um eine Lehrprobe. Es war mir eine große Ehre!" },
+          { src: "/fotos/japan/2009/siggis-fotos-183-min.jpg", description: "\"INTERNATIONAL BLACK JACKET TEAM\" in KAMAKURA" }
+      ] },
+      { title: "Japan 04/2015", photos: [
+        { src: "/fotos/japan/2015/abendessen_im_ryokan-min.jpg", description: "Abendessen im Ryokan" },
+        { src: "/fotos/japan/2015/als_gaeste_im_setojuku_dojo-min.jpg", description: "Als Gäste im Setojuku-Dojo" },
+        { src: "/fotos/japan/2015/auf_dem_weg_nach_tsumago-min.jpg", description: "Auf dem Weg nach Tsumago" },
+        { src: "/fotos/japan/2015/fuer_die_kamera_zu_schnell-min.jpg", description: "Für die Kamera zu schnell" },  
+        { src: "/fotos/japan/2015/im_kloster_koyasan-min.jpg", description: "Im Kloster Koyasan" },
+        { src: "/fotos/japan/2015/in_matsumoto-min.jpg", description: "In Matsumoto" },
+        { src: "/fotos/japan/2015/in_nara-min.jpg", description: "In Nara" },
+        { src: "/fotos/japan/2015/in_osaka-min.jpg", description: "In Osaka" },
+        { src: "/fotos/japan/2015/jka_friends_mit_seto_sensei-min.jpg", description: "JKA-Friends mit Seto Sensei" },
+        { src: "/fotos/japan/2015/kenjutsu_training_in_osaka-min.jpg", description: "Kenjutsu-Training in Osaka" },
+        { src: "/fotos/japan/2015/kumite_training-min.jpg", description: "Kumite-Training" },
+        { src: "/fotos/japan/2015/scb_reisegruppe_2015-min.jpg", description: "SCB Reisegruppe 2015" },
+        { src: "/fotos/japan/2015/spass_muss_sein_im_bambushain-min.jpg", description: "Spaß muss sein im Bambushain" },
+        { src: "/fotos/japan/2015/training_einmal_anders-min.jpg", description: "Training einmal anders" },
+        { src: "/fotos/japan/2015/unterstuetzung_beim_jion_training-min.jpg", description: "Unterstützung beim Jion-Training" }
+      ] }
     ]
   },
   {
